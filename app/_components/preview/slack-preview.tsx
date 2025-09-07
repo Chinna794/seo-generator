399ec59caf86ab202b1ac980b8f4a8d0917ed020
Luis Alvarez <mrluisfeer@gmail.com>
2025-09-07 17:08:30 -0600

    feat: update SlackPreview to use Link component for title navigation

--- app/_components/preview/slack-preview.tsx
+++ app/_components/preview/slack-preview.tsx
@@ -2,6 +2,7 @@ import { FaviconExample } from "@/components/favicon";
 import { Label } from "@/components/ui/label";
 import { useSeoFormStore } from "@/store/use-seo-form-store";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SlackPreview() {
   const { title, description, url } = useSeoFormStore();
@@ -11,7 +12,9 @@ export default function SlackPreview() {
       <div className="group max-w-[500px] cursor-pointer space-y-2 rounded-r-lg border-l-4 border-neutral-700 bg-neutral-900/50 p-4 pl-4">
         <div className="flex items-center gap-2">
           <FaviconExample color="#2a81fb" className="size-5" />
-          <span className="capitalize text-neutral-500">{title.slice(0, 35)}</span>
+          <Link href={url!} className="capitalize text-neutral-500">
+            {title.slice(0, 35)}
+          </Link>
         </div>
         <p className="font-semibold text-[#1698e4] group-hover:underline">{title}</p>
         <p className="text-neutral-300">{description}</p>
