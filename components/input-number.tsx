802e2f7bda87aa05edb54febbc5408219210ff7b
Luis Alvarez <mrluisfeer@gmail.com>
2025-09-08 17:54:58 -0600

    feat: add InputNumber component for numeric input with increment and decrement buttons

---
+++ components/input-number.tsx
@@ -0,0 +1,30 @@
+import { MinusIcon, PlusIcon } from "lucide-react";
+import React from "react";
+import { Button, Group, Input, Label, NumberField } from "react-aria-components";
+
+export default function InputNumber({ label, ...props }: React.ComponentProps<"input"> & { label: string }) {
+  return (
+    <NumberField defaultValue={parseInt(props.value as string) ?? 0} minValue={0}>
+      <div className="*:not-first:mt-2">
+        <Label className="text-foreground text-sm font-medium">{label}</Label>
+        <Group className="border-input data-focus-within:border-ring data-focus-within:ring-ring/50 data-focus-within:has-aria-invalid:ring-destructive/20 dark:data-focus-within:has-aria-invalid:ring-destructive/40 data-focus-within:has-aria-invalid:border-destructive shadow-xs data-disabled:opacity-50 data-focus-within:ring-[3px] relative inline-flex h-9 w-full items-center overflow-hidden whitespace-nowrap rounded-md border text-sm outline-none transition-[color,box-shadow]">
+          <Button
+            type="button"
+            slot="decrement"
+            className="border-input bg-background text-muted-foreground/80 hover:bg-accent hover:text-foreground -ms-px flex aspect-square h-[inherit] items-center justify-center rounded-s-md border text-sm transition-[color,box-shadow] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+          >
+            <MinusIcon size={16} aria-hidden="true" />
+          </Button>
+          <Input className="bg-background text-foreground w-full grow px-3 py-2 text-center tabular-nums" {...props} />
+          <Button
+            type="button"
+            slot="increment"
+            className="border-input bg-background text-muted-foreground/80 hover:bg-accent hover:text-foreground -me-px flex aspect-square h-[inherit] items-center justify-center rounded-e-md border text-sm transition-[color,box-shadow] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+          >
+            <PlusIcon size={16} aria-hidden="true" />
+          </Button>
+        </Group>
+      </div>
+    </NumberField>
+  );
+}
